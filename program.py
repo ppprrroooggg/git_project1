779476dfebebab8ef2ce81b21173bc0195ed1215
DariaE15-1 <74792345+DariaE15-1@users.noreply.github.com>
2022-01-06 17:55:55 +0300

    Opa-Opa2

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('******* **** со своим гитхабом')
+    print('******* ***')
 
 
 if __name__ == '__main__':
